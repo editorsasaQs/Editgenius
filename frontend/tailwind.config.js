8d3de50be15fb3bd9b1c8b6fead40cc7343de315
editorsasaQs <SAIFABEDIN99@GMAIL.COM>
2025-07-05 13:34:36 +0530

    Create tailwind.config.js

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,12 @@
+export default {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#1DB954", // Spotify green
+        dark: "#121212"
+      }
+    },
+  },
+  plugins: [],
+};
